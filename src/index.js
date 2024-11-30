f8e2bb5bad0290b830d33a6175a82be9adadada4
NugrohoDwiAji <nugrohodwiaji@gmail.com>
2024-11-30 14:41:18 +0800

    perbaikan

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import router from './routes/index.js'  
-import { testConnection } from './dbConennection.js'
-const app = express()
-const port = 3000
-dotenv.config()
+// import express from 'express'
+// import cors from 'cors'
+// import bodyParser from 'body-parser'
+// import dotenv from 'dotenv'
+// import cookieParser from 'cookie-parser'
+// import router from './routes/index.js'  
+// import { testConnection } from './dbConennection.js'
+// const app = express()
+// const port = 3000
+// dotenv.config()
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cookieParser())
 
-testConnection()
+// testConnection()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
-app.use(router)
+// app.use(router)
 
-app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-})
-
-export default app
+// app.listen(port, () => {
+//     console.log(`Example app listening on port http://localhost:${port}`)
+// })
+ 
+// export default app
